cd31df8ee0624ca0e8b432c59c2a50b95b018e38
PoojaKaleeswaran <106293027+PoojaKaleeswaran@users.noreply.github.com>
2022-05-26 11:58:08 +0530

    Changed Print line

--- firstGit.java
+++ firstGit.java
@@ -2,6 +2,6 @@ package First-Project;
 
 public class firstGit {
     public static void main(String[] args) {
-        System.out.println("Welcome to first git example")
+        System.out.println("First git example")
     }
 }
